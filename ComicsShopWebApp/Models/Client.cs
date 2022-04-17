a42acf6000e5e47af124bf5100ea84fe1c7d3f5a
anti00anti <oserytsan@gmail.com>
2022-04-17 17:56:25 +0300

    changes

--- ComicsShopWebApp/Models/Client.cs
+++ ComicsShopWebApp/Models/Client.cs
@@ -14,11 +14,11 @@
         public string Phone { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string ClientPassword { get; set; } = null!;
-        public string? DefaultDeliveryAdress { get; set; }
+        public string? DefaultDeliveryAddress { get; set; }
 
         public virtual ICollection<ClientSupport> ClientSupports { get; set; }
         public virtual ICollection<Order> Orders { get; set; }
 
-        public virtual WebAppUser WebAppUser { get; set; }
+        public virtual WebAppUser WebAppUser { get; set; } = null!;
     }
 }
