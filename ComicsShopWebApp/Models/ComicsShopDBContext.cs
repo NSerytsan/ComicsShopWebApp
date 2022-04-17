cf1b3de9e7ac7d5f2bae0ba00c7c036672f30b72
anti <anti@VIRTUALDRACO>
2022-04-17 04:03:02 -0700

    Add project files.

---
+++ ComicsShopWebApp/Models/ComicsShopDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicsShopWebApp.Models
+{
+    public class ComicsShopDBContext : IdentityDbContext<WebAppUser>
+    {
+        public ComicsShopDBContext(DbContextOptions<ComicsShopDBContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ ComicsShopWebApp/Models/WebAppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ComicsShopWebApp.Models
+{
+    public class WebAppUser : IdentityUser
+    {
+    }
+}
