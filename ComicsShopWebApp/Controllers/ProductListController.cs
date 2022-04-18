4235a68ed24cf0e4e08e80fcc49bcaa361bf2389
NSerytsan <nserytsan@gmail.com>
2022-04-18 14:26:18 +0300

    fix

---
+++ ComicsShopWebApp/Controllers/ProductListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComicsShopWebApp.Controllers
+{
+    public class ProductListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
