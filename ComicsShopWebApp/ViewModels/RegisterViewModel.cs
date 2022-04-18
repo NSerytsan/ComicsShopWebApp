d346da4113869067c6ad15ad97ed4fb05ed73f4a
Nastya Serytsan <nserytsan@gmail.com>
2022-04-18 15:47:47 -0700

    add register

---
+++ ComicsShopWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComicsShopWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Підтвердження паролю")]
+        [Compare("Password", ErrorMessage = "Паролі не співпадають.")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
