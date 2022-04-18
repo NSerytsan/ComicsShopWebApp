4ff4fb09b664defd37d0440d13279c63930dce3d
NSerytsan <nserytsan@gmail.com>
2022-04-18 22:16:49 +0300

    category index page

--- ComicsShopWebApp/Controllers/CategoryController.cs
+++ ComicsShopWebApp/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ComicsShopWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComicsShopWebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ComicsShopDBContext _db;
+
+        public CategoryController(ComicsShopDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> CategoryList = _db.Categories;
+            return View(CategoryList);
         }
     }
 }
