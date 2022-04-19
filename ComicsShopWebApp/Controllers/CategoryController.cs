8fc60f649b6c5fe0e4b6dc6f27ff660765c28e76
NSerytsan <nserytsan@gmail.com>
2022-04-19 18:32:15 +0300

    category cintroller fixes

--- ComicsShopWebApp/Controllers/CategoryController.cs
+++ ComicsShopWebApp/Controllers/CategoryController.cs
@@ -17,5 +17,79 @@ namespace ComicsShopWebApp.Controllers
             IEnumerable<Category> CategoryList = _db.Categories;
             return View(CategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if(CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var CategoryFromDb = _db.Categories.Find(id);
+
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CategoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
--- ComicsShopWebApp/Models/Category.cs
+++ ComicsShopWebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ComicsShopWebApp.Models
 {
@@ -12,9 +13,11 @@ namespace ComicsShopWebApp.Models
 
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="Поле \"Назва Категорії\" має бути заповнене")]
+        [Display(Name =("Назва Категорії"))]
         public string CategoryName { get; set; } = null!;
 
-
+        [Display(Name =("Опис категорії"))]
         public string? CategoryDescription { get; set; }
 
         public virtual ICollection<ProductCategory> ProductCategories { get; set; }
