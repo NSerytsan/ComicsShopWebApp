6d96e4d16fb3df5c6fa2f2b6ced6251ccf14da9c
Nastya Serytsan <nserytsan@gmail.com>
2022-04-21 04:50:51 -0700

    add login specific

--- ComicsShopWebApp/Controllers/UserSupportController.cs
+++ ComicsShopWebApp/Controllers/UserSupportController.cs
@@ -1,15 +1,19 @@
 ﻿using ComicsShopWebApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace ComicsShopWebApp.Controllers
 {
     public class UserSupportController : Controller
     {
         private readonly ComicsShopDBContext _db;
+        private readonly UserManager<User> _userManager;
 
-        public UserSupportController(ComicsShopDBContext db)
+        public UserSupportController(ComicsShopDBContext db, UserManager<User> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -23,8 +27,11 @@ namespace ComicsShopWebApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(UserSupport model)
+        public async Task<IActionResult> Create(UserSupport model)
         {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            model.UserId = userId;
+            model.User = await _userManager.GetUserAsync(this.User);
             if (ModelState.IsValid)
             {
                 _db.UserSupports.Add(model);
