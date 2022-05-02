f1d400ab7313c9f678f6a6db601635440037548c
NSerytsan <nserytsan@gmail.com>
2022-05-02 17:09:27 +0300

    fix

---
+++ ComicsShopWebApp/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComicsShopWebApp.ViewModels
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public bool IsSelected { get; set; }
+    }
+}
